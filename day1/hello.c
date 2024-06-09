3c7a7eb3021d4683be2bb5cf7bf293ad8e72c6e6
Deeshupoojary <128202892+Deeshupoojary@users.noreply.github.com>
2024-06-09 20:07:12 +0530

    Add files via upload

---
+++ day1/hello.c
@@ -0,0 +1,10 @@
+// Input & Output - Say "Hello, World!"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    printf("\"Hello, World!\"");  
+    return 0;
+}
